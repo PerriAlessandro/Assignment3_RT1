d44737a1947c75cfe721a0be114135ef6828e2d7
PerriAlessandro <alessandro.perri@outlook.it>
2022-04-08 09:50:22 +0000

    new sphinx quickstart

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@
 import os
 import subprocess
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('./'))
 subprocess.call('doxygen Doxyfile.in', shell=True)
 
 
